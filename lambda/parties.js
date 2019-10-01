7a0be16da0ec284ef654b88890b8c3747b799ccf
Mike Lock <michael.lockwitz@consensys.net>
2019-10-01 16:13:50 -0600

    Updated token metadata.

--- lambda/parties.js
+++ lambda/parties.js
@@ -7,9 +7,12 @@ exports.handler = function(event, context, callback) {
 
     external_url: "https://rimble-devcon-demo.netlify.com/",
     home_url: "https://rimble-devcon-demo.netlify.com/",
+    external_link: "https://rimble-devcon-demo.netlify.com/",
 
     image: "https://rimble-devcon-demo.netlify.com/images/parties.jpg",
-    image_url: "https://rimble-devcon-demo.netlify.com/images/parties.jpg"
+    image_url: "https://rimble-devcon-demo.netlify.com/images/parties.jpg",
+    num_sales: 2000,
+    background_color: "4E3FCE"
   };
 
   const root = {
